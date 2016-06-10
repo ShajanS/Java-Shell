750fe08685e3aff2338a2181890f5d1708b41d45
lossevki <lossevki@UTORONTO.CA>
2016-06-10 21:20:16 +0000

    Implemented echo command with no input redirection, and ability to type commands into the shell.

--- src/driver/JShell.java
+++ src/driver/JShell.java
@@ -29,10 +29,45 @@
 // *********************************************************
 package driver;
 
-public class JShell {
+import java.util.Scanner;
 
+public class JShell {
+  /*TODO: The methods associated with commands should take a String as their
+  *parameter, and return a string to be printed. This will be done with
+  *System.out.print(), so if there needs to be a newline at the end
+  *it should be added by by the method.*/
+  
+  private JShell(){
+    String outputString;
+    //Get input from the user
+    //Trim the input string
+    Scanner in = new Scanner(System.in);
+    String inputString = in.nextLine().trim();
+    //Check if the input string starts with a currently implemented command
+    //If so, call the relevant method with the rest of the string as its
+    //parameter and store its output.
+    if (inputString.startsWith("echo")){
+      outputString = echo(inputString.substring(4)); //since echo is 4 chars
+    }
+    //If not, output an error message
+    else{
+      outputString = "Error: Invalid command\n";
+    }
+    //Print the string returned by the method.
+    System.out.print(outputString);
+    
+    //Close the Scanner. Note: once the loop is implemented, this must be
+    //outside of it.
+    in.close();
+  }
+  
+  private String echo(String params){
+    //Trim the string and return it, with a newline at the end.
+    return params.trim() + "\n";
+  }
+  
   public static void main(String[] args) {
-    // TODO Auto-generated method stub
+    new JShell();
 
   }
 

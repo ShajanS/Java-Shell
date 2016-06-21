d1ee9bb8f6fb4c81a3ac2aa0395ad7837eb52e3f
lossevki <lossevki@UTORONTO.CA>
2016-06-21 03:17:21 +0000

    Refactored history command, updated CRC cards

---
+++ src/commands/History.java
@@ -0,0 +1,52 @@
+package commands;
+
+public class History implements Command{
+  
+  public String execute(driver.JShell shell, String params){
+    //Create the result string
+    String result = "";
+    //Get the command history from the shell
+    java.util.ArrayList<String> hist = shell.commands;
+    
+    //Trim the parameter string and try to get the start index from it
+    int startIndex = startIndexFromParam(params.trim(), hist.size());
+    
+    //If the start index is not -1,
+    if (startIndex != -1){
+    //Loop over the length of the history array, starting at the start index
+      for (int i = startIndex; i < hist.size(); i++){
+        //Append the counter with the relevant command and a newline to the
+        //result string
+        result += (i+1) + ". " + hist.get(i) + "\n";      
+      }
+    } else{
+      //Otherwise, return an error message
+      result = "Error - Invalid Parameter";
+    }
+    return result;
+     
+  }
+  
+  
+  //Returns the start index specified by the parameter, or -1 if invalid
+  private int startIndexFromParam(String param, int arraySize){
+    int startIndex;
+    //If the string is empty, the start index is 0
+    if (param.isEmpty()){
+      startIndex = 0;
+    } else{
+    //If not, try to parse it to an int
+      try{
+        int paramInt = Integer.parseInt(param);
+    //If this succeeds, the start index is the greater of 0 and
+    //the arraylist size minus the parameter
+        startIndex = Math.max(0, arraySize - paramInt);
+      } catch(NumberFormatException e){
+    //If this fails, return -1.
+        startIndex = -1;
+      }
+    }
+    return startIndex;
+  }
+
+}

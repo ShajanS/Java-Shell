143c3f50cc75fc3504bcc0a922b1ebf978c2f226
sivara57@UTORONTO.CA <sivara57@UTORONTO.CA@21d04c0c-ceed-42f4-a002-adbae1b9c5a5>
2016-06-22 15:21:45 +0000

    uploading man command function

---
+++ Assignment2/src/commands/Man.java
@@ -0,0 +1,45 @@
+package commands;
+
+import java.util.Hashtable;
+
+public class Man implements Command{
+  //temp setup 
+  //
+  public String execute(driver.JShell shell, String params){
+    params = params.trim();
+    String result = "";
+    
+    String n = cmdman.get(params);
+    if (n!= null) {
+      result = (params + n + "\n");
+    }
+    else{
+      result = ("Invalid\n");
+    }
+    
+
+    return result;
+    
+  }
+  private Hashtable<String, String> cmdman = new Hashtable<String, String>();
+  
+  public Man() {
+    cmdman.put("exit", " sample output.");
+    cmdman.put("cd", " sample output.");
+    cmdman.put("echo", " sample output.");
+    cmdman.put("history", " sample output.");
+    cmdman.put("mkdir", " sample output.");
+    cmdman.put("pwd", " sample output.");
+    cmdman.put("ls", " sample output.");
+    cmdman.put("cat", " sample output.");
+    cmdman.put("man", " sample output.");
+    
+  }
+  
+  
+}
+
+
+
+
+

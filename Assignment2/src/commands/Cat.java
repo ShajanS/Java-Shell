9eec08455e4f434203f30b3f72b6c1f0c138c9bf
sivara57@UTORONTO.CA <sivara57@UTORONTO.CA@21d04c0c-ceed-42f4-a002-adbae1b9c5a5>
2016-06-24 13:46:30 +0000

    added some basic design code for cat command

---
+++ Assignment2/src/commands/Cat.java
@@ -0,0 +1,20 @@
+package commands;
+
+import data.File;
+
+public class Cat implements Command {
+  public String execute(driver.JShell shell, String params) {
+    params = params.trim();
+    
+    if (params.length() == 0) {
+      // raise error
+      
+     if () {
+       
+       
+       return file.getContents();
+       
+     }
+    }
+  }
+}

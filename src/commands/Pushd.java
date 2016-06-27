b9e8054c08bf6b0b8b120e3b6323672caa83b02d
odurilak <odurilak@UTORONTO.CA>
2016-06-27 17:58:40 +0000

    Now pushd.java uses the FileSystem class, Sai

--- src/commands/Pushd.java
+++ src/commands/Pushd.java
@@ -7,6 +7,7 @@ public class Pushd implements Command{
   public String execute(data.FileSystem fs, String params){
     String result = "\n";
     //Get the absolute path of the parameter if possible
+    fs.pushToDirStack(fs.getCurrentDirectoryPath());
     String absPath = shell.currDir.absolutePath(params);
     stack.add(0, shell.currDir.getPath());
     //Navigate to the parent directory of the param if possible

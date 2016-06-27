01e13930f00fd54adb84f5986085137f53904a46
sivara57@UTORONTO.CA <sivara57@UTORONTO.CA@21d04c0c-ceed-42f4-a002-adbae1b9c5a5>
2016-06-27 15:41:41 +0000

    added suppress warning

--- Assignment2/src/data/InvalidArgumentException.java
+++ Assignment2/src/data/InvalidArgumentException.java
@@ -2,6 +2,7 @@ package data;
 
 // A class to handle when a user calls on a command accompanied by an
 // invalid argument.
+@SuppressWarnings("serial")
 public class InvalidArgumentException extends Exception{
   
   public InvalidArgumentException(String message){
--- Assignment2/src/data/InvalidCommandException.java
+++ Assignment2/src/data/InvalidCommandException.java
@@ -1,6 +1,7 @@
 package data;
 
 // A class to handle when a user calls on a command that doesn't exist.
+@SuppressWarnings("serial")
 public class InvalidCommandException extends Exception{
   
   public InvalidCommandException(String message){

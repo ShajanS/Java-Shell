cc3080be57c76587a7135dbbadf365d327eb936a
odurilak <odurilak@UTORONTO.CA>
2016-06-27 18:12:24 +0000

    Removed call on stack object because it now uses the File System interface, Sai

--- src/commands/Pushd.java
+++ src/commands/Pushd.java
@@ -8,8 +8,6 @@ public class Pushd implements Command{
     String result = "\n";
     //Get the absolute path of the parameter if possible
     fs.pushToDirStack(fs.getCurrentDirectoryPath());
-    String absPath = shell.currDir.absolutePath(params);
-    stack.add(0, shell.currDir.getPath());
     //Navigate to the parent directory of the param if possible
     try{
       data.Directory parent = data.Directory.navigateToParent(absPath,

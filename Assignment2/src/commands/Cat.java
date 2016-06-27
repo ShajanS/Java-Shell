2059655ebc0710cbc32bc3a76394b51a14bbe387
sivara57@UTORONTO.CA <sivara57@UTORONTO.CA@21d04c0c-ceed-42f4-a002-adbae1b9c5a5>
2016-06-27 03:00:45 +0000

    added java doc

--- Assignment2/src/commands/Cat.java
+++ Assignment2/src/commands/Cat.java
@@ -4,18 +4,25 @@ package commands;
 import data.InvalidPathException;
 
 public class Cat implements Command {
-  
+  // error message string
   private String emptyArgs = "File(s) not found\n";
+  //  cat command output string
   String result = "";
   
-  // still in development phase
-  
+  /** 
+   * Outputs the contents of desired text files in the current directory
+   * @param fs     The filesystem in which to make the directory
+   * @param params The list of file names to output
+   * @return       An error if a file cannot be found, and if found output the
+   *               the content
+   */
   public String execute(data.FileSystem fs, String params) {
     String argNames = params;
     // split the input string at every whitespace and store each word(filename)
     // in an array
     String[] fileNames = argNames.split("\\s+");
-
+    
+    // if no file names are given raise error message
     if (fileNames.length == 0) {
       // raise error
       result = emptyArgs;
@@ -25,17 +32,22 @@ public class Cat implements Command {
       // If it's null then it would fall into the else block
       try {
         //Formating of output (# of line breaks)
+        // iterate through the file names given
         for (int file = 0; file < fileNames.length; file++){
+          // get the file contents for each file
           String currentFile = fs.getFileContents(fileNames[file]);
           int length = fileNames.length;
+          // add one new line break if its the last file
           if (fileNames.length > 1) {
             if (file == length - 1){
               result = result.concat(currentFile) + "\n";
             }
+            // add three new line break in between files
             else{
               result = result.concat(currentFile) + "\n\n\n";
             }
           }
+          // add one new line break if its only one file being displayed
           else{
             result = result.concat(currentFile) + "\n";
           }
@@ -43,9 +55,11 @@ public class Cat implements Command {
         }
       } catch (InvalidPathException e) {
         // TODO Auto-generated catch block
+        // raise error if file name given is not present
         result = emptyArgs;
       }
     }
+    // return result
     return result;    
   }
 

4e9935e0f862e17acca49a8adb4d84a9dca7369b
sivara57 <sivara57@UTORONTO.CA>
2016-06-27 02:36:45 +0000

    updated cat command (added cat reqs)

--- src/commands/Cat.java
+++ src/commands/Cat.java
@@ -5,24 +5,42 @@ import data.InvalidPathException;
 
 public class Cat implements Command {
   
-  private String emptyArgs = "File not found";
+  private String emptyArgs = "File(s) not found\n";
   String result = "";
-  String output = "";
   
   // still in development phase
   
   public String execute(data.FileSystem fs, String params) {
+    String argNames = params;
+    // split the input string at every whitespace and store each word(filename)
+    // in an array
+    String[] fileNames = argNames.split("\\s+");
 
-    if (params.length() == 0) {
+    if (fileNames.length == 0) {
       // raise error
-      output = "Invalid";
+      result = emptyArgs;
     }
     else {
       // Obtain the object under path in fileSystem
       // If it's null then it would fall into the else block
-      String current = fs.getCurrentDirectoryPath();
       try {
-        result = fs.getFileContents(current) + "\n";
+        //Formating of output (# of line breaks)
+        for (int file = 0; file < fileNames.length; file++){
+          String currentFile = fs.getFileContents(fileNames[file]);
+          int length = fileNames.length;
+          if (fileNames.length > 1) {
+            if (file == length - 1){
+              result = result.concat(currentFile) + "\n";
+            }
+            else{
+              result = result.concat(currentFile) + "\n\n\n";
+            }
+          }
+          else{
+            result = result.concat(currentFile) + "\n";
+          }
+          
+        }
       } catch (InvalidPathException e) {
         // TODO Auto-generated catch block
         result = emptyArgs;

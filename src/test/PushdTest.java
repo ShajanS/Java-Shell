6218afe67a76534f7d50cb9c3df8409d9ee43e43
odurilak <odurilak@UTORONTO.CA>
2016-06-28 00:39:03 +0000

    Added test cases for Pushd.java, Sai

---
+++ src/test/PushdTest.java
@@ -0,0 +1,61 @@
+package test;
+
+import static org.junit.Assert.*;
+
+import java.util.Stack;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class PushdTest {
+  
+  commands.Pushd pushd = new commands.Pushd();
+  commands.Cd cd = new commands.Cd();
+  MockFileSystem fs;
+  
+  @Before
+  public void setUp(){
+    // Create a new mock filesystem with a directory named testdir
+    fs = new MockFileSystem();
+    fs.directories.put("testdir", new data.Directory("testdir"));
+    fs.directories.put("testdir2", "testdir");
+    dirStack = new Stack<String>;
+  }
+
+  @Test
+  public void testChangeToExistingDir() {
+	// pushd should be able to change to testdir and return a newline
+    String result = pushd.execute(fs, "testdir");
+    assertEquals(result, "\n");
+    // The new current directory should reflect the change
+    assertEquals(fs.currDir, "testdir");
+  }
+  
+  @Test
+  public void testAddDirectoryToStack() {
+	// pushd should be able to add the current directory to the DirStack
+	pushd.execute(fs, "testdir")
+	// The popped directory should be the same as the current directory
+	assertEquals(fs.currDir(), fs.popFromDirStack());
+  }
+  
+  @Test
+  public void testAddTwoDirectoriesToStack() {
+	// pushd should be able to add multiple directories to the DirStack
+	pushd.execute(fs, "testdir");
+	pushd.execute(fs, "testdir2");
+	// The popped directory should be the same as the current directory
+	assertEquals(fs.currDir(), fs.popFromDirStack());
+  }
+  
+  @Test
+  public void testPushToNonExistingDir(){
+    // pushd should not be able to change to any other directory, and return a
+    // error
+    String result = pushd.execute(fs, "somedir");
+    assertEquals(result, "Error - Directory does not exist\n");
+    // The current directory should not have changed
+    assertEquals(fs.currDir, "");
+  }
+
+}

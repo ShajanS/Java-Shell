e029251d21d3aab29b6d30ef74f31ede330beb4f
sivara57 <sivara57@UTORONTO.CA>
2016-06-28 01:16:53 +0000

    added more comments to each test case for man and cat

--- src/test/ManTest.java
+++ src/test/ManTest.java
@@ -12,11 +12,13 @@ public class ManTest {
 
   @Before
   public void setUp() {
+    // Create a new filesystem to handle the command calls for the test cases
     fs = new MockFileSystem();
   }
 
   @Test
   public void testExecute() {
+    // test a valid command to check if proper documentation gets returned 
     String testOutput = man.execute(fs, "exit");
     String testActual = "exit" + ":" + "\n\texits the program" + "\n";
     assertEquals(testActual, testOutput);
@@ -24,14 +26,18 @@ public class ManTest {
 
   @Test
   public void testEmptyArgs() {
+    // pass the command an empty argument and proceed
     String testOutput = man.execute(fs, "");
+    // command should display an appropriate error message
     String testActual = "Invalid Arguments\n";
     assertEquals(testActual, testOutput);
   }
 
   @Test
   public void testInvalidCMD() {
+    // request the command to output documentation for an invalid command
     String testOutput = man.execute(fs, "movedir");
+    // an error message should be displayed
     String testActual = "Invalid Arguments\n";
     assertEquals(testActual, testOutput);
   }

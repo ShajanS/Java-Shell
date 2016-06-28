e4e2639027a2482df630113eb873c4ef2a7846be
sivara57@UTORONTO.CA <sivara57@UTORONTO.CA@21d04c0c-ceed-42f4-a002-adbae1b9c5a5>
2016-06-28 01:16:53 +0000

    added more comments to each test case for man and cat

--- Assignment2/src/test/CatTest.java
+++ Assignment2/src/test/CatTest.java
@@ -16,27 +16,34 @@ public class CatTest {
 
   @Before
   public void setUp() {
+    // access the JFileSystem allocated in data to accommodate the 
+    // command calls and command setup for this unit testing
     sys = new JFileSystem();
+    // populate the the filesystem with files linked to content
     echo.execute(sys, "Contents in file > file.txt");
     echo.execute(sys, "Contents in file 2 > file2.txt");
     echo.execute(sys, "Contents in file 3 > file3.txt");
-
-
   }
 
   @Test
   public void testExecuteSingleFile() {
+    // passing the command to retrieve a file which is present in the directory
     assertEquals(cat.execute(sys, "file3.txt"), "Contents in file 3 \n");
   }
 
   @Test
   public void testExecuteMultipleFile() {
+    // passing the command to retrieve multiple files which is present 
+    // in the directory
+    // format between the two files should be spaced out appropriately 
     assertEquals(cat.execute(sys, "file3.txt file2.txt"),
         "Contents in file 3 \n\n\n" + "Contents in file 2 \n");
   }
 
   @Test
   public void testExecuteNoFile() {
+    // passing the command to retrieve a files which is not present 
+    // in the directory, and error message should be displayed accordingly 
     assertEquals(cat.execute(sys, ""), "File(s) not found\n");
   }
 

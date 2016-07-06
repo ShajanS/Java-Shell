e7ea58d383bd80521cea24c8bf7cc930b3149194
lossevki@UTORONTO.CA <lossevki@UTORONTO.CA@21d04c0c-ceed-42f4-a002-adbae1b9c5a5>
2016-07-06 03:11:50 +0000

    Bug fixes in Echo and File, Directory given new functionality, grep now recognises the -r flag.

--- Assignment2/src/commands/Grep.java
+++ Assignment2/src/commands/Grep.java
@@ -3,22 +3,56 @@ package commands;
 import data.FileSystem;
 import java.util.regex.Pattern;
 import java.util.regex.Matcher;
+import java.util.regex.PatternSyntaxException;
 import java.util.ArrayList;
 
 public class Grep implements Command {
+  
 
   @Override
   public String execute(FileSystem fs, String params) {
     String result = "";
+    Pattern pattern;
     // Get the first token from the parameters. This will either be the
     // -R flag or the regex.
     params = params.trim();
-    String firstToken = params.substring(0, params.indexOf(' '));
-    // Create a regex Pattern from the given regex
-    Pattern pattern = Pattern.compile(firstToken);
-    // Get the list of file paths from the parameters
-    String pathsFromParams = params.substring(params.indexOf(' ')).trim();
-    ArrayList<String> paths = getPaths(pathsFromParams);
+    int currentSearchIndex = params.indexOf(' ');
+    String firstToken = params.substring(0, currentSearchIndex);
+    // Remove the first token from the parameters, and trim them
+    params = params.substring(currentSearchIndex+1).trim();
+    // If the first token is the -R flag, the second token should be the regex
+    // Remove it from the parameters as well
+    try{
+      if (firstToken.equalsIgnoreCase("-r")){
+        currentSearchIndex = params.indexOf(' ');
+        String regex = params.substring(0, currentSearchIndex);
+        params = params.substring(currentSearchIndex+1).trim();
+        pattern = Pattern.compile(regex);
+      } else{
+        // If not, the first token is the regex
+        pattern = Pattern.compile(firstToken);
+      }
+    } catch (PatternSyntaxException e){
+      // If a valid regex is not formed, return an error message
+      return "Error - Invalid regex";
+    }
+    // Get the list of paths from the parameters
+    ArrayList<String> paths = getPaths(params);
+    // If the -R flag is given, process the paths with grepDirectories
+    if (firstToken.equalsIgnoreCase("-r")){
+      result = grepDirectories(fs, paths, pattern);
+    } else{
+    // Otherwise, process them with grepFiles
+      result = grepFiles(fs, paths, pattern);
+    }
+    // Return the result of whichever is run
+    return result;
+  }
+  
+  // Handles grepping a list of files
+  private String grepFiles(FileSystem fs, ArrayList<String> paths,
+      Pattern pattern){
+    String result = "";
     // For each path,
     for (String path : paths){
       // Try to get the contents of that path
@@ -37,6 +71,49 @@ public class Grep implements Command {
         }
       } catch (data.InvalidPathException e){        
       // If this fails, return an error message
+        return "Error - Invalid path\n";
+      }
+    }
+    return result;
+
+  }
+  
+  private String grepDirectories(FileSystem fs, ArrayList<String> paths,
+      Pattern pattern){
+    String result = "";
+    // For each path,
+    for (String path : paths){
+      // Try to get the list of files in the directory
+      try{
+        // Loop through the list of files
+        data.Directory currDir = fs.getDirectory(path);
+        ArrayList<String> fileNames = currDir.getFiles();
+        for (String file : fileNames){
+          // If the directory name doesn't end with a slash, add one. Add the
+          // filename to get its full path.
+          if (!path.endsWith("/")){
+            path += "/";
+          }
+          String fullPath = path + file;
+          // Get the contents of the file
+          String fileContents = fs.getFileContents(fullPath);
+          // Split it by newlines
+          String[] lines = fileContents.split("\n");
+          // For each line, if it matches the regex,
+          // Add the file path, the line, and a newline of the result string
+          for (String line : lines){
+            Matcher matcher = pattern.matcher(line);
+            if (matcher.matches()){
+              result += fullPath + ": " + line + "\n";
+            }
+          }
+          // Run this method on the list of subdirectories and append the
+          // output to the result string
+          result += grepDirectories(fs, currDir.getSubdirNames(), pattern);
+        }
+      
+      } catch(data.InvalidPathException e){
+      // If this fails, return an error message.
         return "Error - Invalid path\n";
       }
     }

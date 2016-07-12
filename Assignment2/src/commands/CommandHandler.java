aad98ec65234de3a881f9ab7c3772b0ee41498df
lossevki@UTORONTO.CA <lossevki@UTORONTO.CA@21d04c0c-ceed-42f4-a002-adbae1b9c5a5>
2016-07-12 04:02:09 +0000

    Nearly completed system to redirect any command's output.

--- Assignment2/src/commands/CommandHandler.java
+++ Assignment2/src/commands/CommandHandler.java
@@ -1,5 +1,6 @@
 package commands;
 
+import java.lang.reflect.InvocationTargetException;
 import java.util.ArrayList;
 
 public class CommandHandler {
@@ -18,22 +19,70 @@ public class CommandHandler {
   // without the redirection.
   public String determineOutputDirection(String params){
     // Check if there are any > in the parameters.
+    if (params.contains(">")){
       // If so, split the string in two at the first one's index
+      int firstBracketLocation = params.indexOf('>');
       // The first part will be the parameters without redirection,
       // The second will be the redirection type and output location.
+      String redirectInfo = params.substring(firstBracketLocation).trim();
+      params = params.substring(0, firstBracketLocation).trim();
+      int pathStart;
       // If the second part starts with >>,
+      if (redirectInfo.startsWith(">>")){
         // Set to append mode
+        outputMode = APPEND_MODE;
         // Note where the rest of the string starts
+        pathStart = 2;
+      } else{
       // If not, it must start with >.
         // Set to overwrite mode
+        outputMode = OVERWRITE_MODE;
         // Note where the rest of the string starts 
+        pathStart = 1;
+      }
       // Get the rest of the string, trim it, and
       // set the output location to it
+      outputLocation = redirectInfo.substring(pathStart);
+    } else{
       // If not, set to standard output mode,
+      outputMode = STDOUT_MODE;
       // and there should be no output location.
+      outputLocation = null;
+    }
     // Return the parameters without the redirection info
-    
-    
+    return params;    
+  }
+  
+  // Handling the output. Returns anything that needs to be printed to the shell.
+  public String handleOutput(data.FileSystem fs, String output){
+    // If in standard output mode, simply return the string.
+    if (outputMode == STDOUT_MODE){
+      return output;
+    } else{
+      // Otherwise, select the appropriate FileSystem method for the current mode
+      String methodNameToUse;
+      if (outputMode == APPEND_MODE){
+        methodNameToUse = "appendToFile";
+      } else {
+        methodNameToUse = "overwriteFile";
+      }
+      // If the output string is not empty or entirely whitespace,
+      if (!output.trim().isEmpty()){
+       // Call the relevant method to put the output string into the given path.
+       // If this succeeds, return an empty string.
+       // If this fails, return an error message.
+       try {
+         java.lang.reflect.Method methodToUse = fs.getClass()
+             .getMethod(methodNameToUse, "".getClass(), "".getClass());
+         methodToUse.invoke(fs, outputLocation, output);
+        } catch (NoSuchMethodException | SecurityException |
+            IllegalAccessException | IllegalArgumentException |
+            InvocationTargetException e) {
+          return "Error - Could not create file.";
+        }      
+      }
+      return "";
+    }
   }
   
   //Getting paths from the parameters

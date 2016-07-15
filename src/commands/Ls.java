e0b00b611406e4974ba0f0a6b1a4e40ed89ea773
kabaniza <kabaniza@UTORONTO.CA>
2016-07-15 18:35:24 +0000

    Made changes to LS to help implement recursive listing, Zain

--- src/commands/Ls.java
+++ src/commands/Ls.java
@@ -15,35 +15,103 @@ public class Ls implements Command {
    */
   public String execute(data.FileSystem fs, String params) {
     String result = "\n";
-    // if no parameters are given then use the current directory or file
     if (params.isEmpty()) {
       result = runWithoutParams(fs);
     } else {
-      // assume that the parameter is a directory
-      boolean directory = true;
-      try {
-        // name then elements in the directory
-        result += fs.getDirectory(params).getName() + ":"
-            + runWithParams(fs, params) + "\n";
-      } catch (InvalidPathException e) {
-        directory = false;
-      }
-      // if it isn't a directory then check if it's a file
-      if (directory == false) {
-        try {
-          // check if it is a file else throw and error
-          fs.getFileContents(params);
-          // split the path and get last element as the name of the file
-          result += params.substring(params.lastIndexOf('/') + 1) + "\n";
-          // if not a valid path for directory or file then return error message
-        } catch (InvalidPathException e) {
-          result = "No such path exists";
+      String [] param = params.split("\\s+");
+      if (param.length == 1) {
+        if (!checkParamsForR(param[0])) {
+          if (checkIfDirectory(fs, param[0])) {
+            result += runWithParams(fs, param[0]);
+          } else if (checkIfFile(fs, params)) {
+            result += getLastElement(param[0]);
+          } else {
+            return "\nNo such path exists";
+          }
+        } else if (checkParamsForR(param[0])) {
+          
+            
+          
         }
+      } else if (param.length > 1) {
+        if (!checkParamsForR(param[0])) {
+          for (String par : param) {
+            result += execute(fs, par);
+          }
+        } else {
+          for (String par : param) {
+            if (par != param[0]) {
+              result += execute(fs, "-r" + par);
+            }
+          }
+        }
+        
       }
+      
+      
+      
+      
     }
-
+    
+    
     return result;
   }
+
+  private boolean checkIfDirectory(data.FileSystem fs, String path) {
+    boolean typeD = true;
+    try {
+      fs.getDirectory(path);
+    } catch (InvalidPathException e) {
+      typeD = false;
+    }
+    return typeD;
+  }
+
+  private boolean checkIfFile(data.FileSystem fs, String path) {
+    boolean typeF = true;
+    try {
+      fs.getFileContents(path);
+    } catch (InvalidPathException e) {
+      typeF = false;
+    }
+    return typeF;
+  }
+
+  private String getLastElement(String path) {
+    String result = path.substring(path.lastIndexOf('/') + 1) + "\n";
+    return result;
+  }
+
+  private boolean checkParamsForR(String param) {
+    boolean assume = false;
+    if (param == "-R" || param == "-r") {
+      assume = true;
+    }
+    return assume;
+  }
+
+  /*
+   * public String execute(data.FileSystem fs, String params) { String result =
+   * "\n"; // if no parameters are given then use the current directory or file
+   * if (params.isEmpty()) { result = runWithoutParams(fs); } else { String[]
+   * param = params.split("\\s+"); if (param.length == 1 && (param[0] != "-R" ||
+   * param[0] != "-r")) { // begin // assume that the parameter is a directory
+   * boolean directory = true; try { // name then elements in the directory
+   * result += fs.getDirectory(params).getName() + ":" + runWithParams(fs,
+   * params); } catch (InvalidPathException e) { directory = false; } // if it
+   * isn't a directory then check if it's a file if (directory == false) { try {
+   * // check if it is a file else throw and error fs.getFileContents(params);
+   * // split the path and get last element as the name of the file result +=
+   * params.substring(params.lastIndexOf('/') + 1) + "\n"; // if not a valid
+   * path for directory or file then return error // message } catch
+   * (InvalidPathException e) { result = "No such path exists"; } // end }
+   * 
+   * } else if (param.length == 1 && (param[0] == "-R" || param[0] == "-r")) {
+   * 
+   * } }
+   * 
+   * return result; }
+   */
 
   /**
    * Formats directory's elements without begin given a parameter
@@ -95,4 +163,7 @@ public class Ls implements Command {
     return result;
   }
 
+ /* private String recursive(data.FileSystem fs) {
+
+  }*/
 }
